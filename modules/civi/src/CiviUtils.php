50930463ebdc78e19a641a92922f3aad322c15cf
Nicol <vingle@users.noreply.github.com>
2020-09-30 01:05:24 +0200

    Update CiviUtils.php
    Somehow this works, but probably not in a good way! Short-term fix to demonstrate/test Wordpress.

--- modules/civi/src/CiviUtils.php
+++ modules/civi/src/CiviUtils.php
@@ -3,7 +3,7 @@
 class CiviUtils {
 
   public static function init() {
-    require_once JPATH_ROOT . '/components/com_civicrm/civicrm.settings.php';
+#   require_once JPATH_ROOT . '/components/com_civicrm/civicrm.settings.php';
     require_once 'CRM/Core/ClassLoader.php';
     CRM_Core_ClassLoader::singleton()->register();
     require_once 'PEAR.php';
